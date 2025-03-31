4dc27bdbe7a61e5f98c9dc01508aae2d58843ba2
William Garrard <willga@umich.edu>
2025-03-31 13:49:32 -0400

    Lab 16 attempt 1

--- lab16.cpp
+++ lab16.cpp
@@ -1,1 +1,14 @@
 // Code to implement the gcf function goes here
+int gcf(int a, int b){
+  if (b > a) {
+    int c=a;
+    a=b;
+    b=c;
+    }
+    while (b != 0) {
+      int remainder =a%b;
+      a=b;
+      b=remainder;
+      }
+      return a;
+      }
